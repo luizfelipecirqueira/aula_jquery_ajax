79befae1c26a52f5bae187b4482d0566bfe82389
luizfelipecirqueira <lfcs18ts@gmail.com>
2020-07-09 19:54:54 -0300

    Commit de atualização do projeto jquery_ajax
    Commit com inclusão de bootstrat, criação de mais um arquivo html para prática no projeto e acréscimo do jquery.

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function consultaCep(){
+    var cep = document.getElementById("cep").value;
+    var url = "https://viacep.com.br/ws/" + cep + "/json/";
+    console.log(url);
+    $.ajax({
+        url: url,
+        type:"GET",
+        success: function(response){
+            console.log(response);
+            document.getElementById("logradouro").innerHTML = response.logradouro;
+            document.getElementById("bairro").innerHTML = response.bairro;
+            document.getElementById("localidade").innerHTML = response.localidade;
+            document.getElementById("uf").innerHTML = response.uf;
+            //alert(response.logradouro);
+        }
+    })
+}
+
+//. pega a classe, # pega o id
